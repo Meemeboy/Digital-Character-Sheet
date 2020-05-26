47d467cd6754557e1e522d2615f83df6fcfe8620
Sam Roberts <giratina5055@gmail.com>
2020-05-26 16:52:57 +0000

    Basic Ability Loading

--- Character_Sheet.py
+++ Character_Sheet.py
@@ -1,26 +1,73 @@
 
 class PlayerClass:
-  def __init__(self,hitdice,saving,skills,abilities,spells):
+  def __init__(self,hitdice,saving,skills,abilities,spells,playerchoice): ## add subclass
     self.hitdice = hitdice
     self.saving = saving
     self.skills = skills
     self.abilities = abilities
+    self.playerchoice = playerchoice
+    mods = [0,0,0,0,0,0]
 
+class Subclass:
+  def __init__(self):
+    return True
+
+
+class Race:
+  def __init__(self,bonus,ability):
+    self.ability = ability
 
 class Player:
-  def __init__(self,xp,hp,stats):
+  def __init__(self,xp,hp,stats,pc,race,statsmod):
     self.xp = xp
     self.hp = hp
+    self.pc = pc
+    self.stats = stats##str,dex,con,int,wis,cha
+    self.statsmod = statsmod
+    self.ac = 10
+    self.acMod = 0
+    self.toHit = 0
+    self.toHitMod = 0
   def returnLevel(self): ##return total level
     level = 0
     xpamounts = [0,300,900,2700,6500,14000,23000,34000,48000,64000,85000,100000,120000,140000,165000,195000,225000,265000,305000,355000]
     for i in range(20):
-      if self.xp < i:
+      if self.xp < xpamounts[i]:
         break
-      self.xp = self.xp - xpamounts[i]
       level = level + 1
     return level
-    
+  def showAbilities(self):
+    for i in range(self.returnLevel()):
+      print(self.pc.abilities[i])
+      print("\n")
+    print(self.pc.abilities[20])
+  def levelStat(self):
+    print("1-Strength \n2-Dexterity \n3-Constitution \n4-Intelligence \n5-Wisdom \n6-Charisma")
+    choice1 = int(input("What stat would you like to increase?(1-6)"))
+    choice2 = int(input("What stat would you like to increase?"))
+    self.stats[choice1 - 1]= self.stats[choice1 - 1] + 1
+    self.stats[choice2 - 1]= self.stats[choice2 - 1] + 1
+
+  def levelUp(self,level):
+    print(self.pc.abilities[level-1])
+    for i in self.pc.playerchoice[level-1]:
+        print(i)
+        print("\n")
+    if self.pc.playerchoice[level-1][0] == "CHS1":
+      choices = 1
+    for i in range(choices):
+      choice = int(input("Please select an option from the above choices"))
+      if self.pc.playerchoice[level-1][choice][0] == "A":
+        mod = int(self.pc.playerchoice[level-1][choice][2])
+        self.acMod = self.acMod + mod
+      if self.pc.playerchoice[level-1][choice][0] == "H":
+        mod = int(self.pc.playerchoice[level-1][choice][2]) ##Finds modifier from string
+        self.toHitMod = self.toHitMod + mod
+      if self.pc.playerchoice[level-1][choice][0] == "Z":
+        self.pc.abilities[20].append(self.pc.playerchoice[level-1][choice])
+
+
+
 
 
 def loadClass(filename):
@@ -32,11 +79,14 @@ def loadClass(filename):
     skills = skillstemp.split()
     abilities = []
     spells = []
+    playeractions = []
     for i in range(20):
       abilities.append(f.readline().split("£"))
+      playeractions.append(f.readline().split("£"))
     for i in range(9):
       spells.append(f.readline().split("£"))
-  return PlayerClass(hitdice,saving,skills,abilities,spells)
+    abilities.append([])
+  return PlayerClass(hitdice,saving,skills,abilities,spells,playeractions)
 
 def createClass():
   try:
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import Character_Sheet as cha
 paladin = cha.loadClass("test")
-erika = cha.Player(1000,10,[])
+erika = cha.Player(1000,10,[10,10,10,10,10,10],paladin,"test",[0,0,0,0,0,0])
 print(erika.returnLevel())
+erika.levelUp(2)
+erika.levelStat()
+erika.showAbilities()
 
 
 
